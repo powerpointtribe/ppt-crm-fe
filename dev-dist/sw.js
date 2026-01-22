e2049aad6cc0fb875356ba3ad4b6fb7d081a592b
ThankGod George <thankgod@flutterwavego.com>
2026-01-22 21:12:07 +0100

    fix

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-137dedbd'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.teono82t31g"
+    "revision": "0.75j714uu0q8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
